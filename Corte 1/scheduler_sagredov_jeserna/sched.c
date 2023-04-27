0fa09abdfa2d96631cf10ab43bc15d2e3d38f76f
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-04-27 01:16:50 -0500

    Added the finished time

--- Corte 1/scheduler_sagredov_jeserna/sched.c
+++ Corte 1/scheduler_sagredov_jeserna/sched.c
@@ -142,6 +142,7 @@ void schedule(list *processes, priority_queue *queues, int nqueues)
     {
       // Levar a la lista finished de su cola de prioridad
       current_process->state = FINISHED;
+      current_process->finished_time = current_time;
       current_queue->finished = push_back(current_queue->finished, current_process);
       // Restar uno al total de procesos que falta por simular
       n--;
