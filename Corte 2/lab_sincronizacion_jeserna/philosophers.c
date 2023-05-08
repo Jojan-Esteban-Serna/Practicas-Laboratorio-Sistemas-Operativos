1e338548436c9004c22c1fee12a98cd153cc9004
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-05-07 22:43:43 -0500

    Update philosophers.c

--- Corte 2/lab_sincronizacion_jeserna/philosophers.c
+++ Corte 2/lab_sincronizacion_jeserna/philosophers.c
@@ -3,20 +3,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+/**
+ * @brief Definicion del tipo semaforo
+ *
+ */
 typedef sem_t semaphore;
 
+/**
+ * @brief Alias para sem_wait
+ *
+ */
 #define down sem_wait
+/**
+ * @brief Alias para sem_post
+ *
+ */
 #define up sem_post
 /**
  * @brief Cantidad de filosofos
  *
  */
+
 int n;
 /**
  * @brief Bandera para indicar el fin de
- * 
+ *
  */
 int finished;
+
+semaphore mutex; // Se inicializan con sem_init
+
+/**
+ * @brief Definicion del tipo semaforo
+ *
+ */
+typedef sem_t semaphore;
+
+/**
+ * @brief Estado de un filosofo
+ *
+ */
+enum State
+{
+    HUNGRY,
+    THINKING,
+    EATING
+};
+
 /**
  * @brief Hilo de filosofos
  *
@@ -25,11 +58,33 @@ int finished;
  */
 void *philosopher(void *arg);
 
+/**
+ * @brief Toma los tenedores
+ *
+ * @param i Indice del filosofo que va a tomar los tenedores
+ */
+void take_forks(int i);
+/**
+ * @brief Pone los tenedores
+ *
+ * @param i indice del filosofo que va a poner los tenedores
+ */
+void put_forks(int i);
+
+/**
+ * @brief Chequea si los filosofos de al lado no estan comiendo
+ *
+ * @param i Indice del filosofo que chequea
+ */
+void test(int i);
+
 int main(int argc, char const *argv[])
 {
     pthread_t *t; // Arreglo de filosofos
     int *ids;     // Arreglo de identificadores de filosofos
-    n = 0;
+    n = 0;        // Numero de filosofos
+    sem_init(&mutex, 0, 1);
+
     // Validar la cantidad de filosofos
     if (argc == 2)
     {
@@ -54,15 +109,15 @@ int main(int argc, char const *argv[])
         pthread_create(&t[i], NULL, philosopher, (void *)&ids[i]);
     }
     sleep(5);
-    //Indicar que la simulacion termino
+    // Indicar que la simulacion termino
     finished = 1;
 
-    //Esperar que los hilos terminen
+    // Esperar que los hilos terminen
     for (int i = 0; i < n; i++)
     {
         pthread_join(t[i], NULL);
     }
-    
+
     exit(EXIT_SUCCESS);
 }
 
@@ -74,6 +129,36 @@ void *philosopher(void *arg)
     printf("Philosopher %d started\n", i);
     while (!finished)
     {
+        down(&mutex);
+        state[i] = HUNGRY;
+        test(i);
+        up(&mutex);
+        down(&s[i]);
     }
     printf("Philosopher %d finished\n", i);
+}
+
+void take_forks(int i)
+{
+    down(&mutex);
+    state[i] = HUNGRY;
+    test(i);
+    up(&mutex);
+    down(&s[i]);
+}
+void put_forks(int i)
+{
+    down(&mutex);
+    state[i] = THINKING;
+    test(LEFT);
+    test(RIGHT);
+    up(&mutex);
+}
+void test(int i)
+{
+    if (state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING)
+    {
+        state[i] = EATING;
+        up(&s[i]);
+    }
 }
