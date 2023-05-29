adfa863ae953018e257c8d487d7ec25f12a09ff8
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-05-29 02:21:14 -0500

    Added the single threaded code

--- Corte 2/webserver_sockets_sagredov_jeserna/client.c
+++ Corte 2/webserver_sockets_sagredov_jeserna/client.c
@@ -1,9 +1,9 @@
 
 /**
-* @file
-* @brief Servidor
-* @author Erwin Meza Vega <emezav@unicauca.edu.co>
-*/
+ * @file
+ * @brief Servidor
+ * @author Erwin Meza Vega <emezav@unicauca.edu.co>
+ */
 
 #include <arpa/inet.h>
 #include <netinet/ip.h>
@@ -13,102 +13,176 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <unistd.h>
-
+#include <limits.h>
+#ifndef PATH_MAX
+#include <linux/limits.h>
+#endif
 #include "util.h"
 
 /**
-* @brief Manejador de señales.
-* @param num Numero de la señal recibida
-* @note El manejador debe ser instalado mediante sigaction(2).
-*/
+ * @brief Manejador de señales.
+ * @param num Numero de la señal recibida
+ * @note El manejador debe ser instalado mediante sigaction(2).
+ */
 void signal_handler(int num);
 
 /** @brief Controla la ejecucion del servidor */
 int finished = 0;
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[])
+{
 
-	//Socket del servidor
-	int s;
+	// Socket del servidor
+	int server_socketfd;
 
-	//Conexion del cliente
-	int c;
+	// Conexion del cliente
+	int client_socketfd;
 
-	//Direccion IPv4 del servidor
+	// Direccion IPv4 del servidor
 	struct sockaddr_in addr;
 
-	//Estructura de accion de señal
+	// Estructura de accion de señal
 	struct sigaction act;
 
-	//Buffer de lectura de la entrada estandar
+	// Buffer de lectura de la entrada estandar
 	char inbuf[BUFSIZ];
-	//Buffer para leer datos desde el socket del servidor
-	char buf[BUFSIZ];
+	// Buffer para escribir datos en el socket del servidor
 
-	ssize_t nread; //Count of bytes read.
-	ssize_t nwritten; //count of bytes written.
+	char server_write_buf[BUFSIZ];
+	// Buffer para leer datos desde el socket del servidor
 
+	char server_read_buf[BUFSIZ];
 
-	//Inicializar la estructura con ceros (NULL)
+	ssize_t nread;	  // Count of bytes read.
+	ssize_t nwritten; // count of bytes written.
+
+	// Inicializar la estructura con ceros (NULL)
 	memset(&act, 0, sizeof(struct sigaction));
 
-	//Establecer el manejador de la señal
+	// Establecer el manejador de la señal
 	act.sa_handler = signal_handler;
-	
-	//Instalar el manejador de la señal SIGINT
+
+	// Instalar el manejador de la señal SIGINT
 	sigaction(SIGINT, &act, NULL);
 
-	//Instalar el manejador de la señal SIGTERM
+	// Instalar el manejador de la señal SIGTERM
 	sigaction(SIGTERM, &act, NULL);
 
-	//1. Crear un socket IPv4, flujo (stream)
-	s = socket(AF_INET, SOCK_STREAM, 0);
+	// 1. Crear un socket IPv4, flujo (stream)
+	server_socketfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	if (s < 0) {
+	if (server_socketfd < 0)
+	{
 		perror("Socket");
 		exit(EXIT_FAILURE);
-		}
+	}
 
-	//Inicializar la direccion antes de conectarse
+	// Inicializar la direccion antes de conectarse
 	memset(&addr, 0, sizeof(struct sockaddr_in));
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(3000); //TODO argumento de linea de comandos!!!
-	if (inet_aton("127.0.0.1", &addr.sin_addr) != 1) { //Conectarse a la direction TODO del servidor
+	addr.sin_port = htons(1234); // TODO argumento de linea de comandos!!!
+	if (inet_aton("127.0.0.1", &addr.sin_addr) != 1)
+	{ // Conectarse a la direction TODO del servidor
 		fprintf(stderr, "Unable to parse the address\n");
 		exit(EXIT_FAILURE);
 	}
 
-	//2. Conectarse a la direccion addr de tipo IPv4  (bind)
-	if (connect(s, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) != 0) {
+	// 2. Conectarse a la direccion addr de tipo IPv4  (bind)
+	if (connect(server_socketfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) != 0)
+	{
 		perror("bind");
 		exit(EXIT_FAILURE);
-		}
-
-	//POST: Connection established.
-	printf("Client connected.\n");
-
-	while(!finished) {
-
-        //Leer el nombre del archivo de la entrada estandar
-        //Construir y enviar el mensaje GET /ruta_archivo HTTP/1.1\n\n
-        //Leer la respuesta
-        //Si la respuesta es 200 OK:
-        //Leer el encabezado del mensaje (hasta encontrar \n\n)
-        //Despues del encabezado se recibe el contenido del archivo
-        //Leer el contenido del archivo y guardar dentro del directorio files/
-
 	}
 
-	//3. Cerrar la conexion con el servidor
-	close(s);
+	// POST: Connection established.
+	printf("Client connected.\n");
+
+	while (!finished)
+	{
+
+		// Leer el nombre del archivo de la entrada estandar
+		printf("Enter the file name: ");
+		scanf("%s", inbuf);
+		// Construir y enviar el mensaje GET /ruta_archivo HTTP/1.1\n\n
+		sprintf(server_write_buf, "GET /%s HTTP/1.1\n\n", inbuf);
+		nwritten = write(server_socketfd, server_write_buf, strlen(server_write_buf));
+		if (nwritten < 0)
+		{
+			finished = 1;
+			continue;
+		}
+		// Leer la respuesta
+		nread = read(server_socketfd, server_read_buf, BUFSIZ);
+		if (nread < 0)
+		{
+			finished = 1;
+			continue;
+		}
+		// Si la respuesta es 404 Not Found:
+		if (strstr(server_read_buf, "404 Not Found") != NULL)
+		{
+			printf("%s", server_read_buf);
+			finished = 1;
+			continue;
+		}
+		// Si la respuesta es 200 OK:
+		if (strstr(server_read_buf, "200 OK") != NULL)
+		{
+			// Leer el encabezado del mensaje (hasta encontrar \n\n)
+			char *end_header = strstr(server_read_buf, "\n\n");
+			// Despues del encabezado se recibe el contenido del archivo
+			// Leer el contenido del archivo y guardar dentro del directorio files/
+			// Apuntar a donde inicia el archivo
+			char *file_content = end_header + 2;
+			// Crear la ruta del archivo
+			char file_path[PATH_MAX];
+			// crearla (inbuf tenia lo ingresado por teclado)
+			sprintf(file_path, "files/%s", inbuf);
+			printf("Saving file %s\n", file_path);
+			//Se crea el directorio files
+			char * folder_name = "files";
+			//Si no existe el directorio files se crea
+			if(access(folder_name, F_OK) == -1){
+				//Se crea el directorio files
+				mkdir(folder_name, 0700);
+				printf("Created folder\n");
+			}
+			printf("Checked folder\n");
+			// abrir el archivo (se crea si no existe), se abre en modo append
+			FILE *file = fopen(file_path, "a+");
+			if (file == NULL)
+			{
+				perror("fopen");
+				continue;
+			}
+			printf("Opened file\n");
+			// escribir el contenido del archivo se agregara al final
+			fprintf(file, "%s", file_content);
+			printf("Saved the first block\n");
+			// leer el resto del archivo del servidor y escribirlo en el archivo local
+			while (nread = read(server_socketfd, server_read_buf, BUFSIZ) > 0)
+			{
+				// escribir el contenido del archivo se agregara al final
+				fwrite(server_read_buf, 1, nread, file);
+			}
+
+			fclose(file);
+			printf("File %s saved.\n", file_path);
+			finished = 1;
+		}
+	}
+
+	// 3. Cerrar la conexion con el servidor
+	close(server_socketfd);
 	printf("Client finished.\n");
 
 	exit(EXIT_SUCCESS);
 }
 
-
-void signal_handler(int num) {
+void signal_handler(int num)
+{
 	printf("Signal %d received\n", num);
 	finished = 1;
 }
--- Corte 2/webserver_sockets_sagredov_jeserna/server.c
+++ Corte 2/webserver_sockets_sagredov_jeserna/server.c
@@ -1,136 +1,275 @@
 
 /**
-* @file
-* @brief Servidor HTTP
-* @author Erwin Meza Vega <emezav@unicauca.edu.co>
-*/
+ * @file
+ * @brief Servidor HTTP
+ * @author Erwin Meza Vega <emezav@unicauca.edu.co>
+ */
 
 #include <arpa/inet.h>
+#include <fcntl.h>
 #include <netinet/ip.h>
+#include <regex.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "util.h"
-
+#include "split.h"
 /**
-* @brief Manejador de señales.
-* @param num Numero de la señal recibida
-* @note El manejador debe ser instalado mediante sigaction(2).
-*/
+ * @brief Manejador de señales.
+ * @param num Numero de la señal recibida
+ * @note El manejador debe ser instalado mediante sigaction(2).
+ */
 void signal_handler(int num);
 
 /** @brief Controla la ejecucion del servidor */
 int finished = 0;
 
-int main(int argc, char * argv[]) {
-
-	//Socket del servidor
+int main(int argc, char *argv[])
+{
+#pragma region Configuraciones
+	// Socket del servidor
 	int s;
 
-	//Conexion del cliente
-	int c;
+	// Conexion del cliente
+	int client_socketfd;
 
-	//Direccion IPv4 del servidor
+	// Direccion IPv4 del servidor
 	struct sockaddr_in addr;
 
-	//Estructura de accion de señal
+	// Estructura de accion de señal
 	struct sigaction act;
 
-	//Buffer de lectura
-	char buf[BUFSIZ];
+	// Buffer de lectura
+	char read_buf[BUFSIZ];
 	ssize_t nread;
 	ssize_t nwritten;
 
-	//Inicializar la estructura con ceros (NULL)
+	// Inicializar la estructura con ceros (NULL)
 	memset(&act, 0, sizeof(struct sigaction));
 
-	//Establecer el manejador de la señal
+	// Establecer el manejador de la señal
 	act.sa_handler = signal_handler;
-	
-	//Instalar el manejador de la señal SIGINT
+
+	// Instalar el manejador de la señal SIGINT
 	sigaction(SIGINT, &act, NULL);
 
-	//Instalar el manejador de la señal SIGTERM
+	// Instalar el manejador de la señal SIGTERM
 	sigaction(SIGTERM, &act, NULL);
 
-	//1. Crear un socket IPv4, flujo (stream)
+	// 1. Crear un socket IPv4, flujo (stream)
 	s = socket(AF_INET, SOCK_STREAM, 0);
 
-	if (s < 0) {
+	if (s < 0)
+	{
 		perror("Socket");
 		exit(EXIT_FAILURE);
-		}
+	}
 
-
-	//Inicializar la direccion antes de asociarla
+	// Inicializar la direccion antes de asociarla
 	memset(&addr, 0, sizeof(struct sockaddr_in));
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(1234); //TODO argumento de linea de comandos!!!
-	if (inet_aton("0.0.0.0", &addr.sin_addr) != 1) { //Escuchar en la direccion 	ip QUE SE ME HAYA ASIGNADO
+	addr.sin_port = htons(1234); // \TODO argumento de linea de comandos!!!
+	if (inet_aton("0.0.0.0", &addr.sin_addr) != 1)
+	{ // Escuchar en la direccion 	ip QUE SE ME HAYA ASIGNADO
 		fprintf(stderr, "Unable to parse the address\n");
 		exit(EXIT_FAILURE);
 	}
 
-	//2. Asociar el socket a la direccion addr de tipo IPv4  (bind)
-	if (bind(s, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) != 0) {
+	// 2. Asociar el socket a la direccion addr de tipo IPv4  (bind)
+	if (bind(s, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) != 0)
+	{
 		perror("bind");
 		exit(EXIT_FAILURE);
-		}
-
-
-	//3 Poner el socket disponible para recibir conexiones
-	if (listen(s, 10) != 0) {
-		perror("listen");
-		exit(EXIT_FAILURE);
-		}
-	printf("Server started.\n");
-		printf("Cuanto vale BUFSIZ? %d\n", BUFSIZ);
-
-	//4. Esperar (BLOQUEARSE) por una conexion
-	c = accept(s, NULL, 0); //TODO opcional: recibir la direccion del cliente
-	if (c < 0) {
-		perror("accept");
-		exit(EXIT_FAILURE);
-		}
-
-	/* TODO crear un hilo que realice la siguiente tarea: */
-
-	while(!finished) {
-		//Leer de la conexion del cliente
-		nread = read(c, buf, BUFSIZ);
-		if (nread <=0) {
-			//End read loop
-			finished = 1;
-			continue;
-		}
-		//La solicitud tiene formato GET /ruta_archivo HTTP/1.1\n\n
-		// Buscar el archivo dentro del subdirectorio www/
-		//Si no existe, construir el mensaje de error (HTTP/1.1 404 Not Found) y enviar
-		//En Caso contrario:
-		//Encontrar el tamaño del archivo (stat)
-		//Construir el encabezado
-		//Enviar el encabezado seguido de \n\n
-		//Enviar el contenido del archivo
-		//Cerrar la conexion y terminar el hilo		
 	}
 
-	//5. Cerrar la conexion con el cliente
-	close(c);
+	// 3 Poner el socket disponible para recibir conexiones
+	if (listen(s, 10) != 0)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+	printf("Server started.\n");
 
-	//6. Cerrar el socket del servidor
+	// 4. Esperar (BLOQUEARSE) por una conexion
+	client_socketfd = accept(s, NULL, 0); // \TODO opcional: recibir la direccion del cliente
+	if (client_socketfd < 0)
+	{
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+#pragma endregion
+	/* TODO crear un hilo que realice la siguiente tarea: */
+
+	while (!finished)
+	{
+		// Leer de la conexion del cliente
+		nread = read(client_socketfd, read_buf, BUFSIZ);
+		if (nread > 0)
+		{
+			// La solicitud tiene formato GET /ruta_archivo HTTP/1.1\n\n
+			printf("Client sended: %s\n", read_buf);
+
+			// separar la solicitud en partes
+			split_list *parts = split(read_buf, " ");
+			// Verificar que la solicitud tenga el formato correcto
+			if (parts->count != 3)
+			{
+				finished = 1;
+				continue;
+			}
+			// Verificar que la solicitud sea GET
+			if (!equals(parts->parts[0], "GET"))
+			{
+				finished = 1;
+				continue;
+			}
+			// Verificar que la solicitud ttenga http/1.1
+			if (!equals(parts->parts[2], "HTTP/1.1\n\n"))
+			{
+				finished = 1;
+				continue;
+			}
+			char *filename = parts->parts[1];
+			char path[BUFSIZ];
+			sprintf(path, "www%s", filename);
+			printf("Archivo solicitado: %s\n", path);
+			//  Buscar el archivo dentro del subdirectorio con (stat) www/
+			struct stat stat_buffer;
+			int stat_result = stat(path, &stat_buffer);
+			if (stat_result < 0)
+			{
+				printf("Archivo no encontrado\n");
+				// Si no existe, construir el mensaje de error (HTTP/1.1 404 Not Found) y enviar
+				char html_not_found_response[BUFSIZ];
+				sprintf(html_not_found_response,
+						"<!DOCTYPE html>\n"
+						"<html lang=\"en\">\n"
+						"<head>\n"
+						"<meta charset=\"utf-8\">\n"
+						"<title>Error</title>\n"
+						"</head>\n"
+						"<body>\n"
+						"<pre>Cannot GET %s</pre>\n"
+						"</body>\n"
+						"</html>",
+						path);
+				char header_html_not_found_response[BUFSIZ];
+				time_t current_time;
+				time(&current_time);
+
+				// Convert the time to a string representation
+				char *time_string = ctime(&current_time);
+
+				sprintf(header_html_not_found_response,
+						"HTTP/1.1 404 Not Found\n"
+						"X-Powered-By: OS HTTP Server\n"
+						"Content-Type: text/html; charset=utf-8\n"
+						"Content-Length: %d\n"
+						"Date: Wed, %d\n"
+						"Connection: close",
+						strlen(html_not_found_response), time_string);
+
+				char *response_string = malloc((strlen(header_html_not_found_response) + strlen(html_not_found_response) + 1) * sizeof(char));
+				sprintf(response_string, "%s\n\n%s", header_html_not_found_response, html_not_found_response);
+				// Enviar el mensaje de error
+				nwritten = write(client_socketfd, response_string, strlen(response_string));
+				free(response_string);
+				if (nwritten < 0)
+				{
+					finished = 1;
+					continue;
+				}
+			}
+			// POST el archivo existe
+
+			// En Caso contrario:
+			// Encontrar el tamaño del archivo (stat)
+			printf("Archivo encontrado\n");
+			off_t file_size = stat_buffer.st_size;
+
+			// Construir el encabezado
+			char header_response[BUFSIZ];
+			time_t current_time;
+			time(&current_time);
+			char success_response[BUFSIZ];
+			sprintf(success_response,
+					"HTTP/1.1 200 OK\n"
+					"X-Powered-By: OS HTTP Server\n"
+					"Content-Type: %s\n"
+					"Content-Length: %d\n"
+					"Date: Wed, %d\n\n",
+					get_extension(filename), file_size, ctime(&current_time));
+			int headr_size = strlen(success_response);
+			int remaining_space = BUFSIZ - headr_size - 1;
+			success_response[BUFSIZ - 1] = '\0'; // El cliente recibe un primer mensaje en el cual hace fprintf
+
+			// Enviar el encabezado seguido de \n\n
+			// Abrir el archivo
+			FILE *file_to_send = fopen(path, "r");
+			if (file_to_send == NULL)
+			{
+				finished = 1;
+				continue;
+			}
+			// leer el archivo en bloques de BUFSIZ desde donde termina el encabezado
+			nread = fread(success_response + headr_size, sizeof(char), remaining_space, file_to_send);
+			if (nread < 0)
+			{
+				finished = 1;
+				continue;
+			}
+			// enviar el primer bloque
+			nwritten = write(client_socketfd, success_response, strlen(success_response));
+			if (nwritten < 0)
+			{
+				finished = 1;
+				continue;
+			}
+			printf("written the first %d bytes\n", nwritten);
+			char file_buffer[BUFSIZ];
+			// Leer el archivo en bloques de BUFSIZ mientras haya datos
+			if (file_size > BUFSIZ - headr_size)
+			{
+				while (nread = fread(file_buffer, sizeof(char), BUFSIZ, file_to_send) > 0)
+				{
+					printf("read %d bytes\n", nread);
+					// escrbir el bloque en el socket
+					//  Enviar el contenido del archivo
+
+					nwritten = write(client_socketfd, file_buffer, nread);
+					if (nwritten < 0)
+					{
+						finished = 1;
+						break;
+					}
+					printf("written %d bytes\n", nwritten);
+				}
+			}else{
+				finished = 1;
+			}
+			// Cerrar la conexion y terminar el hilo
+		}
+	}
+
+	// 5. Cerrar la conexion con el cliente
+	close(client_socketfd);
+
+	// 6. Cerrar el socket del servidor
 	close(s);
 	printf("Server finished.\n");
 
 	exit(EXIT_SUCCESS);
 }
 
-
-void signal_handler(int num) {
+void signal_handler(int num)
+{
 	printf("Signal %d received\n", num);
 	finished = 1;
 }
