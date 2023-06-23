adaba541f94d55dab4ab20367cbd91563af8f54a
Jojan-Esteban-Serna <jeserna@unicauca.edu.co>
2023-06-23 01:21:50 -0500

    AAdded the last project code base

---
+++ Corte 3/final_sagredov_jeserna/README.txt
@@ -0,0 +1,5 @@
+Elaborado por: XXX <yyy@unicauca.edu.co>
+
+Deberá desarrollar un programa en C, que tome como entrada el primer sector de un disco duro
+inicializado como “MBR” y con una tabla de particiones válida, e imprimir la información de la
+tabla de particiones.
---
+++ Corte 3/final_sagredov_jeserna/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[]) {
+
+	//TODO Imprimir la tabla de particiones!!
+
+	//La tabla de particones se encuentra a un desplazamiento de 446 bytes
+	//a partir del inicio del sector de arranque y ocupa exactamente 64 bytes
+
+	//Despues de la tabla de particiones, se encuentran los bytes 0xAA55.
+
+	exit(EXIT_SUCCESS);
+
+	}
